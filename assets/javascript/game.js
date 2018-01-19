f596be9c429ec9a4ecc5c8042e03e5e160e20c46
Mark Mikelonis <mark.mikelonis@gmail.com>
2018-01-18 18:41:13 -0800

    created function to mitigate DRY

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,42 +7,50 @@ var userGuess = " ";
 		var compGuess;
 		var guessArray = [];
 		
+		function cGuess(){
+			return charArray[Math.floor(Math.random() * 26)];
+		}
+		compGuess = cGuess();
 		
-		 compGuess = charArray[Math.floor(Math.random() * 26)];
-		 document.onkeyup = function(element){
-		 		userGuess = element.key;
-		 		
-		 		console.log("compGuess: " + compGuess);
-		 	
-			 	for ( var i = 0; i<charArray.length;i++){
-			 		if (userGuess === charArray[i] 
-			 			&& guessArray.indexOf(userGuess) === -1 
-			 			&& userGuess !== compGuess){
-			 			console.log(userGuess + " " + compGuess);
-			 			guessesLeft--;
-			 			losses++;
-			 			guessArray.push(userGuess);
-			 			console.log(userGuess);
-			 		} else if (userGuess === compGuess){
-			 			wins++;
-			 			reset();
-			 		}
+		document.onkeyup = function(element){
+			 		userGuess = element.key;
+			 		
+			 		console.log("compGuess: " + compGuess);
+			 	
+				 	for ( var i = 0; i<charArray.length;i++){
+				 		if (userGuess === charArray[i] 
+				 			&& guessArray.indexOf(userGuess) === -1 
+				 			&& userGuess !== compGuess){
+				 			console.log(userGuess + " " + compGuess);
+				 			guessesLeft--;
+				 			
+				 			guessArray.push(userGuess);
+				 			if (guessesLeft <= 0){
+				 				losses++;
+				 				reset();
+				 			}
+				 			console.log(userGuess);
+				 		} else if (userGuess === compGuess){
+				 			wins++;
+				 			reset();
+				 		}
+				 	}
+			 	function reset() {
+			 		userGuess ="";
+			 		guessesLeft = 9;
+			 		guessArray = [];
+			 		compGuess = cGuess();
+			 		console.log("compGuess: " + compGuess);
 			 	}
-		 	function reset() {
-		 		userGuess ="";
-		 		guessesLeft = 9;
-		 		guessArray = [];
-		 		compGuess = charArray[Math.floor(Math.random() * 26)];
-		 		console.log("compGuess: " + compGuess);
-		 	}
-		 	// console.log(guessArray);
-		 		
+			 	
+			 		
 
-			var html =
-	          "<p>Wins " + wins + "</p>" +
-	          "<p>Losses: " + losses + "</p>" +
-	          "<p>Guesses Left: " + guessesLeft + "</p>" +
-	          "<p>You guessed so far: " + guessArray.toString() + "</p>";
+				var html =
+		          "<p>Wins: " + wins + "</p>" +
+		          "<p>Losses: " + losses + "</p>" +
+		          "<p>Guesses Left: " + guessesLeft + "</p>" +
+		          "<p>You guessed so far: " + guessArray.toString() + "</p>";
 
-	        document.querySelector("#hangman").innerHTML = html; 
-	    }     
+		        document.querySelector("#hangman").innerHTML = html; 
+		    }	 	 
+	         
